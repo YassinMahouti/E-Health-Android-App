df53c84e887b8f12283b61c60282d4f60c23f361
iliassboutechdat <iliass.boutechdat@student.ehb.be>
2020-04-25 16:04:39 +0200

    Master push git

---
+++ app/src/main/java/com/example/iliasspush/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.iliasspush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import java.net.URL;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);     //on Vieuw activity_main.xml openen
+    }
+    private EditText txtUsername;
+    private EditText txtAge;
+    private Button btnConfirm;
+
+
+
+
+    public void intit(){
+        txtUsername = (EditText) findViewById(R.id.txtNameInput);
+        txtAge = (EditText) findViewById(R.id.txtAgeInput);
+
+    }
+}
