a4ef908e5f7e183eb299ca293fb7b82b7a28bd1f
32485 <yassin.mahouti@student.ehb.be>
2020-04-27 14:43:55 +0200

    Mockups geimporteerd (login en start activity) in een navdrawer project. Linken zijn gemaakt tussen elke activity.

--- src/main/java/be/ehb/LoginMockup/LoginAct.java
+++ src/main/java/be/ehb/LoginMockup/LoginAct.java
@@ -3,8 +3,11 @@ package be.ehb.LoginMockup;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import be.ehb.haseb.R;
@@ -12,11 +15,23 @@ import be.ehb.haseb.R;
 public class LoginAct extends AppCompatActivity {
 
     ImageView backButton;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginAct.this, NavDrawerAct.class);
+                startActivity(intent);
+            }
+        });
+
 
         backButton = findViewById(R.id.backBtn);
         backButton.setOnClickListener(new View.OnClickListener() {
