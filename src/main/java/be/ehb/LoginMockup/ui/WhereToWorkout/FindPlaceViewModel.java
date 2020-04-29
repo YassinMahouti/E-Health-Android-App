f56e60ea4ef1e796ea8f71e15d9cc46652aa725e
32485 <yassin.mahouti@student.ehb.be>
2020-04-29 17:43:28 +0200

    Navigatie bar verderwerken

---
+++ src/main/java/be/ehb/LoginMockup/ui/WhereToWorkout/FindPlaceViewModel.java
@@ -0,0 +1,20 @@
+package be.ehb.LoginMockup.ui.WhereToWorkout;
+
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class FindPlaceViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public FindPlaceViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Find Place fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
