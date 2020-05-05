27cf43ecc23544a714fec9506c330ae3809b02ca
Nanilabrute <komi.daniel.anani@student.ehb.be>
2020-05-05 16:37:06 +0200

    first version

---
+++ app/src/main/java/be/ehb/LoginMockup/LoginAct.java
@@ -0,0 +1,43 @@
+package be.ehb.LoginMockup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import be.ehb.Ehealth.R;
+
+public class LoginAct extends AppCompatActivity {
+
+    ImageView backButton;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.button);
+
+        //ga naar home
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginAct.this, NavDrawerAct.class);
+                startActivity(intent);
+            }
+        });
+
+
+        backButton = findViewById(R.id.backBtn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
