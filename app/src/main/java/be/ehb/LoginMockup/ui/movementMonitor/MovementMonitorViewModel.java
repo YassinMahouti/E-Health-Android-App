58cf61c3fdfec8f6f2c86e63c9bd2d50e02ac7ea
Nanilabrute <komi.daniel.anani@student.ehb.be>
2020-05-05 20:45:44 +0200

    "add movement monitor to nav"

---
+++ app/src/main/java/be/ehb/LoginMockup/ui/movementMonitor/MovementMonitorViewModel.java
@@ -0,0 +1,7 @@
+package be.ehb.LoginMockup.ui.movementMonitor;
+
+import androidx.lifecycle.ViewModel;
+
+public class MovementMonitorViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
