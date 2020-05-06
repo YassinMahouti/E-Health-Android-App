dca9b05c1b7505b4c2ef392abaf03cc6276a30d1
NaniLabrute <63151036+NaniLabrute@users.noreply.github.com>
2020-05-06 16:22:20 +0200

    Delete MovementMonitorViewModel.java

--- app/src/main/java/be/ehb/LoginMockup/ui/movementMonitor/MovementMonitorViewModel.java
+++
@@ -1,7 +0,0 @@
-package be.ehb.LoginMockup.ui.movementMonitor;
-
-import androidx.lifecycle.ViewModel;
-
-public class MovementMonitorViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
