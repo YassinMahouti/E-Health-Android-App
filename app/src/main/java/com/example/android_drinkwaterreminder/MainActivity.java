1d555c1e667ed93edba9e7fe10ecefc730f7f3a1
iliassboutechdat <iliass.boutechdat@student.ehb.be>
2020-05-07 18:14:24 +0200

    Drinkwater Reminder v 1.0
    Code but not functionl yet
    Notification doesn't pop up
    Swich button doesn't work correctly

--- app/src/main/java/com/example/android_drinkwaterreminder/MainActivity.java
+++ app/src/main/java/com/example/android_drinkwaterreminder/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.android_drinkwaterreminder;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 
@@ -16,27 +19,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String CHANNEL_ID ="Notification" ;
+    private static final String CHANNEL_ID = "1";
     Switch switch_state;
     EditText txt_weight;
     Button confirm_btn;
-    boolean save=false;
+    boolean save = false;
 
-    public void intitialize(){      //intitialisation of android screen vraiables
+    public void intitialize() {      //intitialisation of android screen vraiables
         switch_state = findViewById(R.id.switch_status);
         txt_weight = findViewById(R.id.Txt_weight);
-        confirm_btn=findViewById(R.id.btn_confirm);
-        if (switch_state.isChecked()) {
-            save = true;
-        }
+        confirm_btn = findViewById(R.id.btn_confirm);
+
     }
 
-    public double calcToDrinkWater(int weight){     //methode to calculate amount of to drink water
-        double toDrink = weight*0.033;
+    public double calcToDrinkWater(int weight) {     //methode to calculate amount of to drink water
+        double toDrink = weight * 0.033;
         return toDrink;
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "DrinkWaterReminder";
+            String description = "Reminds to Drink water every 2 hours";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -45,9 +65,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         intitialize();
-        if (save == true) {
-            txt_weight.setVisibility(View.VISIBLE);
-        }
+        switch_state.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true) {
+                    txt_weight.setVisibility(View.VISIBLE);
+                    confirm_btn.setVisibility(View.VISIBLE);
+                } else {
+                    txt_weight.setVisibility(View.INVISIBLE);
+                    confirm_btn.setVisibility(View.INVISIBLE);
+                }
+                createNotificationChannel();
+            }
+        });
+
 
 
         //set notification
@@ -58,38 +89,47 @@ public class MainActivity extends AppCompatActivity {
                 String value = txt_weight.getText().toString();
                 int user_weight = Integer.parseInt(value);
 
-                //set time to notif
-                LocalTime currentTime = LocalTime.now();
-                LocalTime currentTimeAdvanced = LocalTime.now();
 
-                while (save == true) {
-                    while (currentTime.getHour() >= 1 && currentTime.getHour() <= 7) {
-                        if (currentTime == currentTimeAdvanced) {
-                            //notification
-                            String message = "Drink water now";
-                            NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                                    MainActivity.this
-                            )
-                                    .setSmallIcon(R.drawable.ic_drinkwater)
-                                    .setContentTitle("Reminder")
-                                    .setContentText(message)
-                                    .setAutoCancel(true);
 
-                            Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            intent.putExtra("message", message);
 
-                            PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-                            builder.setContentIntent(pendingIntent);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        //set time to notif
+                        Date date = new Date();   // given date
+                        Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+                        calendar.setTime(date);   // assigns calendar to given date
+                        final int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
 
-                            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                            notificationManager.notify(0, builder.build());
+                        while (switch_state.isChecked()) {
+                            while (currentHour >= 1 && currentHour <= 7) {
+                                //notification
+                                String message = "Drink water now";
+                                NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                                        MainActivity.this,CHANNEL_ID
+                                )
+                                        .setSmallIcon(R.drawable.ic_drinkwater)
+                                        .setContentTitle("Reminder")
+                                        .setContentText(message)
+                                        .setAutoCancel(true);
 
-                            //move next notification until 2 hours from current notif
-                            currentTimeAdvanced = currentTime.plusHours(2);
+                                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                intent.putExtra("message", message);
+
+                                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                                builder.setContentIntent(pendingIntent);
+
+                                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                                notificationManager.notify(0, builder.build());
+
+
+                            }
                         }
                     }
-                }
+                }, 1*60);
+
 
             }
         });
