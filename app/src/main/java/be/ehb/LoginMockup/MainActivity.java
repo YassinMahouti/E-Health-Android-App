c5860e4964deadd655496894f20f901f403731a3
Yassin <yassin.mahouti@student.ehb.be>
2020-05-09 12:56:42 +0200

    alles

---
+++ app/src/main/java/be/ehb/LoginMockup/MainActivity.java
@@ -0,0 +1,48 @@
+package be.ehb.LoginMockup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import be.ehb.Ehealth.R;
+import be.ehb.LoginMockup.ui.registratie.RegAct;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button loginButton;
+    Button regButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //initialise registration button
+        regButton = findViewById(R.id.register);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegAct.class);
+                startActivity(intent);
+            }
+        });
+
+        loginButton = findViewById(R.id.loginBtn);
+
+
+
+        //go to login screen
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginAct.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
