2e341676848ba572f75876fb2ed0aa694d2e0b40
Yassin <yassin.mahouti@student.ehb.be>
2020-05-12 19:27:41 +0200

    alles

---
+++ app/src/main/java/be/ehb/LoginMockup/ui/Calorie/CalorieCalc.java
@@ -0,0 +1,20 @@
+package be.ehb.LoginMockup.ui.Calorie;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import be.ehb.Ehealth.R;
+
+public class CalorieCalc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie_calculator);
+
+
+
+
+    }
+}
