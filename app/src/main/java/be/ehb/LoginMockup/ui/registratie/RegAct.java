2bc7ffd06b61a2ef96fc1e3d1938a2aa9d77b793
Branche_Bart <Bart.de.backer1@student.ehb.be>
2020-05-13 18:47:12 +0200

    User authenticatie koppelen aan database, gegevens van de user worden nu ingeschreven in de realtime database wanneer hij inlogt. Samen gemaakt met Amer en Cedric heeft geholpen met debuggen.

--- app/src/main/java/be/ehb/LoginMockup/ui/registratie/RegAct.java
+++ app/src/main/java/be/ehb/LoginMockup/ui/registratie/RegAct.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +18,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import be.ehb.Ehealth.R;
 import be.ehb.LoginMockup.LoginAct;
@@ -31,10 +32,10 @@ public class RegAct extends AppCompatActivity {
     Button myRegisterBtn;
     TextView myLoginBtn;
     FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
+    DatabaseReference mRefValues;
     ProgressBar progressBar;
     ImageView backButton;
-    RadioButton radioGenderFemale, radioGenderMale;
-
 
 
     @Override
@@ -51,11 +52,11 @@ public class RegAct extends AppCompatActivity {
         myLoginBtn = findViewById(R.id.createText);
         myWeight = findViewById(R.id.weight);
         myHeight = findViewById(R.id.height);
-        radioGenderFemale =(RadioButton)findViewById(R.id.radio_female);
-        radioGenderMale =(RadioButton)findViewById(R.id.radio_male);
+
 
         firebaseAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar);
+        final FirebaseDatabase mRoot = FirebaseDatabase.getInstance();
 
 
         if (firebaseAuth.getCurrentUser() == null) {
@@ -67,15 +68,16 @@ public class RegAct extends AppCompatActivity {
 
 
 
-
-
-
-
         myRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = myEmail.getText().toString().trim();
                 String password = myPassword.getText().toString().trim();
+
+                final String name = myName.getText().toString().trim();
+                final String phone = myPhone.getText().toString().trim();
+
+
 
                 if (TextUtils.isEmpty(email)) {
                     myEmail.setError("Email is required");
@@ -93,21 +95,32 @@ public class RegAct extends AppCompatActivity {
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
+
                 firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            Member member= new Member(
+                                    name,
+                                    email,
+                                    phone
+                            );
+                            databaseReference = mRoot.getReference("Members");
+                            mRefValues = mRoot.getReference("member");
+                            mRefValues = databaseReference.child(firebaseAuth.getCurrentUser().getUid());
+                            mRefValues.setValue(member);
+                            System.out.println(member.getEmail() +member.getName() +member.getPhone());
+                             /* FirebaseDatabase.getInstance().getReference("Members")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(member);*/
                             Toast.makeText(RegAct.this, "User Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            startActivity(new Intent(getApplicationContext(),LoginAct.class));
                         } else {
                             Toast.makeText(RegAct.this, "Something went wrong :/" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
                         }
-                    }
+                        }
                 });
-
-
-
             }
         });
 
