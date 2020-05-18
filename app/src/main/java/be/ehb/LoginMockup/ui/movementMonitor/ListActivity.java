8bb6c0cab71eeeee1f79e4174420d9f2a547d4e9
Nanilabrute <komi.daniel.anani@student.ehb.be>
2020-05-19 01:11:55 +0200

    bewegingsMonitor

--- app/src/main/java/be/ehb/LoginMockup/ui/movementMonitor/ListActivity.java
+++ app/src/main/java/be/ehb/LoginMockup/ui/movementMonitor/ListActivity.java
@@ -44,96 +44,11 @@ public class ListActivity extends AppCompatActivity {
         monitor = new MovementMonitor(sessionCalories, sessionDuration);
 
         readDataChangeFromFirebase();
-        buildRecycleView(activities);
+        buildRecycleView();
     }
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
-/*
-    private void writDataFromFirebase() {
-        DatabaseReference myRef = database.getReference().child("Activity");
 
-        myRef.child("activity").child("activity_name").setValue("Swimming");
-        myRef.child("activity").child("activity_kcal").setValue(100);
-        myRef.child("activity").child("activity_duration").setValue(10);
-        myRef.child("activity").child("activity_type").setValue("endurance");
-        myRef.child("activity").child("activity_intensity").setValue("hard");
-        //myRef.child("activity").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/swimming.jpg?alt=media&token=66bd5194-707b-489e-b4c6-0aa91057b142");
-        myRef.child("activity").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity1").child("activity_name").setValue("running");
-        myRef.child("activity1").child("activity_kcal").setValue(150);
-        myRef.child("activity1").child("activity_duration").setValue(120);
-        myRef.child("activity1").child("activity_type").setValue("endurance");
-        myRef.child("activity1").child("activity_intensity").setValue("hard");
-        //myRef.child("activity1").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/running.jpg?alt=media&token=f233d1f7-db6f-4940-a352-167a79e79dd1");
-        myRef.child("activity1").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity2").child("activity_name").setValue("push-up");
-        myRef.child("activity2").child("activity_kcal").setValue(200);
-        myRef.child("activity2").child("activity_duration").setValue(60);
-        myRef.child("activity2").child("activity_type").setValue("endurance");
-        myRef.child("activity2").child("activity_intensity").setValue("hard");
-        //myRef.child("activity2").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/push-ups.jpg?alt=media&token=d8325609-1a8b-4aaa-be94-df3fe097ee03");
-        myRef.child("activity2").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity3").child("activity_name").setValue("cycling");
-        myRef.child("activity3").child("activity_kcal").setValue(250);
-        myRef.child("activity3").child("activity_duration").setValue(60);
-        myRef.child("activity3").child("activity_type").setValue("endurance");
-        myRef.child("activity3").child("activity_intensity").setValue("hard");
-        myRef.child("activity3").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        /////////////////////////////////////////////////////////////////////
-        myRef.child("activity4").child("activity_name").setValue("cycling");
-        myRef.child("activity4").child("activity_kcal").setValue(250);
-        myRef.child("activity4").child("activity_duration").setValue(60);
-        myRef.child("activity4").child("activity_type").setValue("endurance");
-        myRef.child("activity4").child("activity_intensity").setValue("hard");
-        myRef.child("activity4").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity5").child("activity_name").setValue("cycling");
-        myRef.child("activity5").child("activity_kcal").setValue(250);
-        myRef.child("activity5").child("activity_duration").setValue(60);
-        myRef.child("activity5").child("activity_type").setValue("endurance");
-        myRef.child("activity5").child("activity_intensity").setValue("hard");
-        myRef.child("activity5").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity6").child("activity_name").setValue("cycling");
-        myRef.child("activity6").child("activity_kcal").setValue(350);
-        myRef.child("activity6").child("activity_duration").setValue(60);
-        myRef.child("activity6").child("activity_type").setValue("endurance");
-        myRef.child("activity6").child("activity_intensity").setValue("hard");
-        myRef.child("activity6").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity7").child("activity_name").setValue("cycling");
-        myRef.child("activity7").child("activity_kcal").setValue(350);
-        myRef.child("activity7").child("activity_duration").setValue(60);
-        myRef.child("activity7").child("activity_type").setValue("endurance");
-        myRef.child("activity7").child("activity_intensity").setValue("hard");
-        myRef.child("activity7").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity8").child("activity_name").setValue("cycling");
-        myRef.child("activity8").child("activity_kcal").setValue(300);
-        myRef.child("activity8").child("activity_duration").setValue(60);
-        myRef.child("activity8").child("activity_type").setValue("endurance");
-        myRef.child("activity8").child("activity_intensity").setValue("hard");
-        myRef.child("activity8").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity9").child("activity_name").setValue("cycling");
-        myRef.child("activity9").child("activity_kcal").setValue(300);
-        myRef.child("activity9").child("activity_duration").setValue(60);
-        myRef.child("activity9").child("activity_type").setValue("endurance");
-        myRef.child("activity9").child("activity_intensity").setValue("hard");
-        myRef.child("activity9").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-
-        myRef.child("activity10").child("activity_name").setValue("cycling");
-        myRef.child("activity10").child("activity_kcal").setValue(400);
-        myRef.child("activity10").child("activity_duration").setValue(60);
-        myRef.child("activity10").child("activity_type").setValue("endurance");
-        myRef.child("activity10").child("activity_intensity").setValue("hard");
-        myRef.child("activity10").child("activity_image").setValue("https://firebasestorage.googleapis.com/v0/b/mybfpapp.appspot.com/o/cycling.jpg?alt=media&token=cf5e959f-de86-4d4e-a4d8-144f308f34e0");
-    }
-*/
     /**
      * this function read the data available in the firebase*/
 
@@ -160,7 +75,7 @@ public class ListActivity extends AppCompatActivity {
                         activities.add(new Activity(activityName, activityType,activityKcal, activityDuration, activityIntensity, activityImage));
                     }
                 }
-                buildRecycleView(activities);
+                buildRecycleView();
             }
 
             @Override
@@ -173,10 +88,10 @@ public class ListActivity extends AppCompatActivity {
         });
     }
 
-    public void buildRecycleView(List activities) {
+    private void buildRecycleView() {
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         ListAdapter listAdapter = new ListAdapter(activities);
-        //recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        //recyclerView.setAdapter(listAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(listAdapter);
     }
 }
