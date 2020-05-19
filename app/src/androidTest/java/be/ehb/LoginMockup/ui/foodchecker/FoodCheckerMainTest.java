9fc2da81ca887409b4956660cc586571ce21ae61
cedricDve <cedric.dve1@gmail.com>
2020-05-19 09:31:18 +0200

    Laatste aanpassingen en toevoeging testen. Toevoegen van maps extensies in corona, Wrokout, placefinder. Toevoegen alle Latlng in o.a apparte klasse voor leesbaarheid. Aanpassingen code + documentatie. Aanpassingen van 17 to 18-05.

--- app/src/androidTest/java/be/ehb/LoginMockup/ui/foodchecker/FoodCheckerMainTest.java
+++ app/src/androidTest/java/be/ehb/LoginMockup/ui/foodchecker/FoodCheckerMainTest.java
@@ -1,23 +1,35 @@
 package be.ehb.LoginMockup.ui.foodchecker;
 
 import android.content.Context;
+import android.widget.TextView;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.EspressoKey;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import be.ehb.Ehealth.R;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
 
-class FoodCheckerMainTest {
+public class FoodCheckerMainTest {
 
-
+        TextView txt_antwoord;
         // Instantiate MainActivity
         FoodCheckerMain mMainActivity;
+
+        @Before
+        public void setup() throws Exception{
+            txt_antwoord = mMainActivity.findViewById(R.id.textViewNutrition);
+        }
 
         @Test
         public void useAppContext() {
@@ -41,5 +53,53 @@ class FoodCheckerMainTest {
             String actual = "Foo";
             assertEquals(expected, actual,"Should be identical to the user's input");
         }
+          //--Test if values or fetched correctly : Pizza
+        @Test
+            public void shouldReturn113Cal(){
+            onView(withId(R.id.editTextInput)).perform(typeText("pizza"));
+            Espresso.closeSoftKeyboard();
+            onView(withId(R.id.buttonFetchFood)).perform(click());
+            onView(withId(R.id.buttonFetchDetails)).perform(click());
+            String expected = "Calories: 113.0" +"\n" +
+                            "Fat: 1.5g"+"\n" +
+                             "Prot: 3.5" +"\n"+
+                            "Carbs: N.a"+"\n" +
+                             "Serving Size: N.A";
+            String actual = String.valueOf(txt_antwoord.getText());
+            assertEquals(expected, actual);
+
+        }
+        //--Test if values or fetched correctly : Cocca cola
+         @Test
+     public void shouldReturn110Cal(){
+        onView(withId(R.id.editTextInput)).perform(typeText("coca cola"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.buttonFetchFood)).perform(click());
+        onView(withId(R.id.buttonFetchDetails)).perform(click());
+        String expected = "Calories: 110.0" +"\n" +
+                "Fat: 0"+"\n" +
+                "Prot: 3.5" +"\n"+
+                "Carbs: N.a"+"\n" +
+                "Serving Size: N.A";
+        String actual = String.valueOf(txt_antwoord.getText());
+        assertEquals(expected, actual);
+
     }
+    //--Test if values or fetched correctly : egg
+    @Test
+    public void shouldReturn60Cal(){
+        onView(withId(R.id.editTextInput)).perform(typeText("egg"));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.buttonFetchFood)).perform(click());
+        onView(withId(R.id.buttonFetchDetails)).perform(click());
+        String expected = "Calories: 60" +"\n" +
+                "Fat: 3.5"+"\n" +
+                "Prot: 5" +"\n"+
+                "Carbs: N.a"+"\n" +
+                "Serving Size: N.A";
+        String actual = String.valueOf(txt_antwoord.getText());
+        assertEquals(expected, actual);
+    }
+}
+
 
